03be142c27ae7b4e8ac1ffa0e4032d1b2868f41f
Axel Frias Hernandez <axel.frias257@gmail.com>
2021-12-26 00:44:23 -0600

    Manejo de variables numericas y de texto

---
+++ NumericCalculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NumericCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+
+            Console.WriteLine("Input the first number: ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the second number: ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The result is: {number1 + number2}");
+        }
+    }
+}
---
+++ StringExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StringExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What's your name? Please write your full name: ");
+            string fullName = Console.ReadLine();
+            Console.WriteLine($"Welcome, {fullName}!");
+        }
+    }
+}
