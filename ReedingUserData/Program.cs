d9216de257a3a64d3f3e407eda6a1b9876ddbba7
Axel Frias Hernandez <axel.frias257@gmail.com>
2021-12-27 23:15:55 -0600

    Reeding user data

---
+++ ReedingUserData/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReedingUserData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program calculates the area of a triangle");
+            
+            //Sides
+            Console.Write("Side A: ");
+            float sideA = float.Parse(Console.ReadLine());
+            
+            Console.Write("Side B: ");
+            float sideB = float.Parse(Console.ReadLine());
+
+            // A = a * b
+            float area = sideA + sideB;
+            Console.WriteLine($"Area = {area}");
+        }
+    }
+}
