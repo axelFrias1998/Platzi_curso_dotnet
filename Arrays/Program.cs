499ac962e3f941d11ca5d3933e1e7b47dba6d0ee
Axel Frias Hernandez <axel.frias257@gmail.com>
2021-12-27 23:29:15 -0600

    Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] coffeeTypes;
+            float[] coffeePrices;
+
+            coffeeTypes = new string[] { "Expresso", "Large", "Latte", "Moka"};
+            coffeePrices = new float[] { 20.4f, 19.5f, 24.5f, 20 };
+
+            for (int i = 0; i < coffeeTypes.Length; i++)
+                Console.WriteLine($"Type: {coffeeTypes[i]} \tCost: ${coffeePrices[i]}");
+            
+            
+        }
+    }
+}
