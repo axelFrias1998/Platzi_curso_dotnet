d17e929e0319b401f4b0dce752fcf8ec95071c2a
Axel Frias Hernandez <axel.frias257@gmail.com>
2021-12-27 23:29:35 -0600

    Something missing

--- LogicalOperators/Program.cs
+++ LogicalOperators/Program.cs
@@ -8,6 +8,7 @@ namespace LogicalOperators
         {
             bool value1 = true;
             bool value2 = false;
+            
             Console.WriteLine($"The result of logical AND is: {value1 && value2}");
             Console.WriteLine($"The result of logical OR is: {value1 || value2}");
             Console.WriteLine($"The result of logical NOT AND is: {!(value1 && value2)}");
