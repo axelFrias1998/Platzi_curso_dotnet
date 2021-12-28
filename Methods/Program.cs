a972dc80ddc723c1f32454c65e79849becbcdc58
Axel Frias Hernandez <axel.frias257@gmail.com>
2021-12-27 23:55:38 -0600

    Methods

---
+++ Methods/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            Console.WriteLine(rnd.Next(1, 10));
+        }
+    }
+}
