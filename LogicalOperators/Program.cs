cc3cfff67bb42e8f223566da36d3662c6f4ba1b1
Axel Frias Hernandez <axel.frias257@gmail.com>
2021-12-27 22:15:22 -0600

    Logical operators

---
+++ LogicalOperators/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool value1 = true;
+            bool value2 = false;
+            Console.WriteLine($"The result of logical AND is: {value1 && value2}");
+            Console.WriteLine($"The result of logical OR is: {value1 || value2}");
+            Console.WriteLine($"The result of logical NOT AND is: {!(value1 && value2)}");
+            Console.WriteLine($"The result of logical NOT OR is: {!(value1 || value2)}");
+        }
+    }
+}
---
+++ SquareArea/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SquareArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program calculates the area of a triangle");
+            
+            //Sides
+            int sideA = 10;
+            float sideB = 20.2f;
+
+            // A = a * b
+            float area = sideA + sideB;
+            Console.WriteLine($"Area = {area}");
+        }
+    }
+}
