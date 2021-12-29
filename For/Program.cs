4c9f416dd2b3fc5f52cf8882f25fc4b2b8a75f01
Axel Frias Hernandez <axel.frias257@gmail.com>
2021-12-29 00:09:41 -0600

    For example

---
+++ For/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 50; i++)
+            {
+                Console.WriteLine("Hola, Platzi " + (i + 1));
+            }
+        }
+    }
+}
