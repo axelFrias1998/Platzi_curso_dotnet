dc82e675db74be798c81e1403f6eba8348a4e4d4
Axel Frias Hernandez <axel.frias257@gmail.com>
2022-01-04 19:02:26 -0600

    Me dio flojera el codigo estructurado, solo vi el video

---
+++ restaurantReservationSystem/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+/*
+    1. Validar si es usuario existente o si se debe registrar, y generar un sistema de registro o login
+    2. El programa debe ser capaz de darle la bienvenida a un usuario si existe
+    3. El programa debe repetirse hasta que se registren las 10 personas
+
+*/
+namespace restaurantReservationSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] userNames = new string[10] { "Axel", string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty,};
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
